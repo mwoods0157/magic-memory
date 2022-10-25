cf89d6db4d674bf91d92378caabcf5fb7588b9f6
Matthew <matthew.woods0157@gmail.com>
2022-10-25 15:39:58 -0400

    Added matching property to card/useeffect

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css'
 import SingleCard from './components/SingleCard';
 
 const cardImages = [
-  {"src": "/img/helmet-1.png"},
-  {"src": "/img/potion-1.png"},
-  {"src": "/img/ring-1.png"},
-  {"src": "/img/scroll-1.png"},
-  {"src": "/img/shield-1.png"},
-  {"src": "/img/sword-1.png"}
+  {"src": "/img/helmet-1.png", matched: false},
+  {"src": "/img/potion-1.png", matched: false},
+  {"src": "/img/ring-1.png", matched: false},
+  {"src": "/img/scroll-1.png", matched: false},
+  {"src": "/img/shield-1.png", matched: false},
+  {"src": "/img/sword-1.png", matched: false}
 ];
 
 function App() {
@@ -35,15 +35,26 @@ function App() {
   //compare 2 selected cards
   useEffect(() => {
     if (choiceOne && choiceTwo) {
+
       if (choiceOne.src === choiceTwo.src) {
-        console.log("Those cards match");
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceOne.src) {
+              return {...card, matched: true}
+            } else {
+              return card
+            }
+          })
+        })
         resetTurn();
       } else {
-        console.log("Those cards didn't match");
+        
         resetTurn();
       }
     }
   }, [choiceOne, choiceTwo])
+
+  console.log(cards);
 
   //reset choices & increase turn
   const resetTurn = () => {
