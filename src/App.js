9a0d540f5a9b288218f2ce3e784c67c12d62792c
Matthew <matthew.woods0157@gmail.com>
2022-10-26 16:42:04 -0400

    Card flipping feature

--- src/App.js
+++ src/App.js
@@ -48,8 +48,7 @@ function App() {
         })
         resetTurn();
       } else {
-        
-        resetTurn();
+        setTimeout(() => resetTurn(), 1000);
       }
     }
   }, [choiceOne, choiceTwo])
@@ -73,7 +72,8 @@ function App() {
           <SingleCard 
           key={card.id} 
           card={card}
-          handleChoice={handleChoice} />
+          handleChoice={handleChoice}
+          flipped={card === choiceOne || card === choiceTwo || card.matched} />
         ))}
       </div>
     </div>
